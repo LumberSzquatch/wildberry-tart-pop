3ef7612fcac4382114c532ba9bc77560f9d9ae4d
Zach Fenton <zfenton@uncomn.com>
2020-02-29 21:46:42 -0600

    lookin like a poptart

---
+++ sample.ts
@@ -0,0 +1,16 @@
+/**
+ * This is a block comment
+ */
+
+export enum SessionGetters {
+  CURRENTUSER = "SessionModule/currentUser",
+  AUTHENTICATED = "SessionModule/authenticated",
+  GROUPS = "SessionModule/groups",
+  ISSITEADMIN = "SessionModule/isSiteAdmin"
+}
+
+// line comment
+async function sample(param: string, marap?: boolean) {
+  const foo = "bar";
+  let poop: Boolean = false;
+}
